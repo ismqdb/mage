baa9e44905f22fbe801ed1bdbdcf22682146acb8
ismqdb <djukic.djukic96@gmail.com>
2025-03-26 07:25:07 +0100

    Cube setup

---
+++ src/02_cube/02_cube.cpp
@@ -0,0 +1,234 @@
+/* **************************************************************************************************** */
+
+#include "./02_cube.hpp"
+
+/* **************************************************************************************************** */
+
+int main(){
+    cubeApp triangleApp;
+
+    triangleApp.gameLoop();
+}
+
+/* **************************************************************************************************** */
+
+cubeApp::cubeApp() : app(){
+    memset(this->pressed, 0, GLFW_KEY_LAST);
+
+    glfwSetup();
+    openglSetup();
+}
+
+cubeApp::~cubeApp() {
+    openglTeardown();
+    glfwTeardown();
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::openglSetup(){
+
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::render(){    
+
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::openglTeardown(){
+
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::gameLoop(){
+    int running = 1;
+
+    mage::shader shaders[] = {
+        {GL_VERTEX_SHADER, "../shaders/02_cube/02_cube.vert"},
+        {GL_FRAGMENT_SHADER, "../shaders/02_cube/02_cube.frag"},
+        {GL_NONE, NULL}
+    };
+
+    program = mage::loadShader(shaders); 
+    glUseProgram(program);
+
+    while (running) {
+        render();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        update();
+
+        running &= (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_RELEASE);
+        running &= (glfwWindowShouldClose(window) != GL_TRUE);
+   }
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::glfwSetup(){
+    glfwInit();
+
+    info.windowWidth = 800;
+    info.windowHeight = 600;
+
+    const char title[] = "mage";
+    info.title = title;
+    
+    #ifdef __APPLE__
+        info.majorVersion = 3;
+        info.minorVersion = 2;
+    #else
+        info.majorVersion = 4;
+        info.minorVersion = 5;
+    #endif
+
+    info.samples = 0;
+    info.flags.all = 0;
+    info.flags.cursor = 1;
+
+    #ifdef _DEBUG
+        info.flags.debug = 1;
+    #endif
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+    window = glfwCreateWindow(info.windowWidth, info.windowHeight, info.title.c_str(), 
+        info.flags.fullscreen ? glfwGetPrimaryMonitor() : NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetWindowUserPointer(window, this);
+
+    glfwSetWindowSizeCallback(window, onResize);
+    glfwSetKeyCallback(window, onKey);
+    glfwSetMouseButtonCallback(window, onMouseButton);
+    glfwSetCursorPosCallback(window, onMouseMove);
+    glfwSetScrollCallback(window, onMouseWheel);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSetKeyCallback(window, onKey);
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::glfwTeardown(){
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::onResize(GLFWwindow* window, int width, int height){
+    cubeApp *pThis = (cubeApp*)glfwGetWindowUserPointer(window);
+    pThis->resizeWindow(width, height);
+}
+
+void cubeApp::resizeWindow(int width, int height){
+    glViewport(0, 0, width, height);
+    aspectRatio = float(width)/float(height);
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::onKey(GLFWwindow* window, int key, int scancode, int action, int mods){
+    cubeApp *pThis = (cubeApp*)glfwGetWindowUserPointer(window);
+    pThis->keyPress(key, scancode, action, mods);
+}
+
+void cubeApp::keyPress(int key, int scancode, int action, int mods){
+    if(key == GLFW_KEY_UNKNOWN)
+        return;
+
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
+        glfwSetWindowShouldClose(window, true);
+    
+    for(int i = 0; i < GLFW_KEY_LAST; i++)
+        if(action == GLFW_PRESS && i == key)
+            pressed[key] = true;
+        else if(action == GLFW_RELEASE && i == key)
+            pressed[key] = false;
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::onMouseButton(GLFWwindow* window, int button, int action, int mods){
+    cubeApp *pThis = (cubeApp*)glfwGetWindowUserPointer(window);
+    pThis->mouseClick(button, action, mods);
+}
+
+void cubeApp::mouseClick(int button, int action, int mods){
+
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::onMouseMove(GLFWwindow* window, double x, double y){
+    cubeApp *pThis = (cubeApp*)glfwGetWindowUserPointer(window);
+    pThis->mouseMove(x, y);
+}
+
+void cubeApp::mouseMove(double x, double y){
+
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::onMouseWheel(GLFWwindow* window, double xoffset, double yoffset){
+    cubeApp *pThis = (cubeApp*)glfwGetWindowUserPointer(window);
+    pThis->mouseWheel(xoffset, yoffset);
+}
+
+void cubeApp::mouseWheel(double xoffset, double yoffset){
+
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::setVsync(int enable){
+    info.flags.vsync = enable ? 1 : 0;
+    glfwSwapInterval((int)info.flags.vsync);
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::getMousePosition(int *x, int *y){
+    double dx, dy;
+    glfwGetCursorPos(window, &dx, &dy);
+
+    *x = (int)(floor(dx));
+    *y = (int)(floor(dy));
+}
+
+/* **************************************************************************************************** */
+
+void GLAPIENTRY
+cubeApp::MessageCallback(GLenum source,
+                 GLenum type,
+                 GLuint id,
+                 GLenum severity,
+                 GLsizei length,
+                 const GLchar* message,
+                 const void* userParam){
+    fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+        ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+            type, severity, message );
+}
+
+/* **************************************************************************************************** */
+
+void cubeApp::update(){
+}
+
+/* **************************************************************************************************** */
---
+++ src/02_cube/02_cube.hpp
@@ -0,0 +1,60 @@
+/* **************************************************************************************************** */
+
+#include "../../headers/app.hpp"
+
+#include <cstring>
+#include <array>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+/* **************************************************************************************************** */
+
+class cubeApp : public mage::app {
+    public:
+        cubeApp();
+        ~cubeApp() override;
+
+        void openglSetup() override;
+        void openglTeardown() override;
+        void render() override;
+        void gameLoop() override;
+        void glfwSetup() override;
+        void glfwTeardown() override;
+        void update() override;
+
+        static void onResize(GLFWwindow*, int, int);
+        virtual void resizeWindow(int, int);
+
+        static void onKey(GLFWwindow*, int, int, int, int);
+        virtual void keyPress(int, int, int, int);
+
+        static void onMouseButton(GLFWwindow*, int, int, int);
+        virtual void mouseClick(int, int, int);
+
+        static void onMouseMove(GLFWwindow*, double, double);
+        virtual void mouseMove(double, double);
+
+        static void onMouseWheel(GLFWwindow*, double, double);
+        virtual void mouseWheel(double, double);
+
+        void setVsync(int);
+        void getMousePosition(int*, int*);
+
+        static void GLAPIENTRY MessageCallback(GLenum,
+            GLenum,
+            GLuint,
+            GLenum,
+            GLsizei,
+            const GLchar*,
+            const void*);
+
+    private:
+        bool pressed[GLFW_KEY_LAST];
+        
+        float aspectRatio;
+        GLuint program;
+};
+
+/* *****************************************************************************************************/

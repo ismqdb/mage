fb1902fda88c2cdbbcc01a27f4ef462e1e01807a
ismqdb <djukic.djukic96@gmail.com>
2025-04-26 22:50:55 +0200

    rdot

---
+++ src/04_rdot/04_rdot.cpp
@@ -0,0 +1,293 @@
+/* **************************************************************************************************** */
+
+#include "./04_rdot.hpp"
+
+/* **************************************************************************************************** */
+
+i32 main(){
+    dotApp dotApp;
+
+    dotApp.gameLoop();
+}
+
+/* **************************************************************************************************** */
+
+dotApp::dotApp() : app(){
+    numVAOs = 1;
+    numBuffers = 1;
+
+    VAOs[0] = {0};
+    buffers[0] = {0};
+
+    numVertices = 0;
+
+    vertices[0][0] = 0.25f;
+    vertices[0][1] = 0.40f;
+
+    numVertices = sizeof(vertices[0])/sizeof(GLfloat) / 2;
+
+    memset(this->pressed, 0, GLFW_KEY_LAST);
+
+    glfwSetup();
+}
+
+dotApp::~dotApp() {
+    glfwTeardown();
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::openglSetup(){
+    glGenVertexArrays(numVAOs, VAOs);
+    glBindVertexArray(VAOs[0]);
+
+    glCreateBuffers(numBuffers, buffers);
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferStorage(GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)(0));
+    glEnableVertexAttribArray(0);
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::render(){
+    static const f32 black[] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+    glClearBufferfv(GL_COLOR, 0, black);
+
+    glBindVertexArray(VAOs[0]);
+    glPointSize(this->pointSize);
+    glDrawArrays(GL_POINTS, 0, numVertices);        // Points
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::openglTeardown(){
+
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::gameLoop(){
+    i32 running = 1;
+
+    mage::shader shaders[] = {
+        {GL_VERTEX_SHADER, "../shaders/04_rdot/04_rdot.vert"},
+        {GL_FRAGMENT_SHADER, "../shaders/04_rdot/04_rdot.frag"},
+        {GL_NONE, NULL}
+    };
+
+    GLuint program = mage::loadShader(shaders);
+    glUseProgram(program);
+
+    while (running) {
+        render();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        update();
+
+        running &= (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_RELEASE);
+        running &= (glfwWindowShouldClose(window) != GL_TRUE);
+   }
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::glfwSetup(){
+    glfwInit();
+
+    info.windowWidth = 800;
+    info.windowHeight = 600;
+
+    const char title[] = "mage";
+    info.title = title;
+    
+    #ifdef __APPLE__
+        info.majorVersion = 3;
+        info.minorVersion = 2;
+    #else
+        info.majorVersion = 4;
+        info.minorVersion = 5;
+    #endif
+
+    info.samples = 0;
+    info.flags.all = 0;
+    info.flags.cursor = 1;
+
+    #ifdef _DEBUG
+        info.flags.debug = 1;
+    #endif
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+    window = glfwCreateWindow(info.windowWidth, info.windowHeight, info.title.c_str(), 
+        info.flags.fullscreen ? glfwGetPrimaryMonitor() : NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetWindowUserPointer(window, this);
+
+    glfwSetWindowSizeCallback(window, onResize);
+    glfwSetKeyCallback(window, onKey);
+    glfwSetMouseButtonCallback(window, onMouseButton);
+    glfwSetCursorPosCallback(window, onMouseMove);
+    glfwSetScrollCallback(window, onMouseWheel);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSetKeyCallback(window, onKey);
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
+
+    openglSetup();
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::glfwTeardown(){
+    openglTeardown();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::onResize(GLFWwindow* window, i32 w, i32 h){
+    dotApp *pThis = (dotApp*)glfwGetWindowUserPointer(window);
+    pThis->resizeWindow(w, h);
+}
+
+void dotApp::resizeWindow(i32 x, i32 y){
+    info.windowWidth = x;
+    info.windowHeight = y;
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::onKey(GLFWwindow* window, i32 key, i32 scancode, i32 action, i32 mods){
+    dotApp *pThis = (dotApp*)glfwGetWindowUserPointer(window);
+    pThis->keyPress(key, scancode, action, mods);
+}
+
+void dotApp::keyPress(i32 key, i32 scancode, i32 action, i32 mods){
+    if(key == GLFW_KEY_UNKNOWN)
+        return;
+
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
+        glfwSetWindowShouldClose(window, true);
+    
+    for(i32 i = 0; i < GLFW_KEY_LAST; i++)
+        if(action == GLFW_PRESS && i == key)
+            pressed[key] = true;
+        else if(action == GLFW_RELEASE && i == key)
+            pressed[key] = false;
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::onMouseButton(GLFWwindow* window, i32 button, i32 action, i32 mods){
+    dotApp *pThis = (dotApp*)glfwGetWindowUserPointer(window);
+    pThis->mouseClick(button, action, mods);
+}
+
+void dotApp::mouseClick(i32 button, i32 action, i32 mods){
+
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::onMouseMove(GLFWwindow* window, f64 x, f64 y){
+    dotApp *pThis = (dotApp*)glfwGetWindowUserPointer(window);
+    pThis->mouseMove(x, y);
+}
+
+void dotApp::mouseMove(f64 x, f64 y){
+
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::onMouseWheel(GLFWwindow* window, f64 xoffset, f64 yoffset){
+    dotApp *pThis = (dotApp*)glfwGetWindowUserPointer(window);
+    pThis->mouseWheel(xoffset, yoffset);
+}
+
+void dotApp::mouseWheel(f64 xoffset, f64 yoffset){
+
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::setVsync(i32 enable){
+    info.flags.vsync = enable ? 1 : 0;
+    glfwSwapInterval((i32)info.flags.vsync);
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::getMousePosition(i32 *x, i32 *y){
+    f64 dx, dy;
+    glfwGetCursorPos(window, &dx, &dy);
+
+    *x = (i32)(floor(dx));
+    *y = (i32)(floor(dy));
+}
+
+/* **************************************************************************************************** */
+
+void GLAPIENTRY
+dotApp::MessageCallback(GLenum source,
+                 GLenum type,
+                 GLuint id,
+                 GLenum severity,
+                 GLsizei length,
+                 const GLchar* message,
+                 const void* userParam){
+
+}
+
+/* **************************************************************************************************** */
+
+void dotApp::update(){
+    for(i32 i = 0; i < GLFW_KEY_LAST; i++){
+        if(!pressed[i])
+            continue;
+
+        switch(i){
+            case GLFW_KEY_LEFT:
+                this->vertices[0][0] -= 0.01;
+                break;
+
+            case GLFW_KEY_RIGHT:
+                this->vertices[0][0] += 0.01;
+                break;
+
+            case GLFW_KEY_UP:
+                this->vertices[0][1] += 0.01;
+                break;
+
+            case GLFW_KEY_DOWN:
+                this->vertices[0][1] -= 0.01;
+                break;
+
+            case GLFW_KEY_A:
+                this->pointSize++;
+                break;
+            
+            case GLFW_KEY_D:
+                this->pointSize--;
+                break;
+        }
+    }
+
+    this->openglSetup();
+}
+
+/* **************************************************************************************************** */
---
+++ src/04_rdot/04_rdot.hpp
@@ -0,0 +1,64 @@
+/* **************************************************************************************************** */
+
+#include "../../headers/app.hpp"
+#include "../../headers/types.hpp"
+
+#include <cstring>
+
+/* **************************************************************************************************** */
+
+class dotApp : public mage::app {
+    public:
+        dotApp();
+        ~dotApp() override;
+
+        void openglSetup() override;
+        void openglTeardown() override;
+        void render() override;
+        void gameLoop() override;
+        void glfwSetup() override;
+        void glfwTeardown() override;
+        void update() override;
+
+        static void onResize(GLFWwindow*, i32, i32);
+        virtual void resizeWindow(i32, i32);
+
+        static void onKey(GLFWwindow*, i32, i32, i32, i32);
+        virtual void keyPress(i32, i32, i32, i32);
+
+        static void onMouseButton(GLFWwindow*, i32, i32, i32);
+        virtual void mouseClick(i32, i32, i32);
+
+        static void onMouseMove(GLFWwindow*, f64, f64);
+        virtual void mouseMove(f64, f64);
+
+        static void onMouseWheel(GLFWwindow*, f64, f64);
+        virtual void mouseWheel(f64, f64);
+
+        void setVsync(i32);
+        void getMousePosition(i32*, i32*);
+
+        static void GLAPIENTRY MessageCallback(GLenum,
+            GLenum,
+            GLuint,
+            GLenum,
+            GLsizei,
+            const GLchar*,
+            const void*);
+
+    private:
+        i32 numVAOs = 1;
+        i32 numBuffers = 1;
+
+        GLuint VAOs[1];
+        GLuint buffers[1];
+
+        GLuint numVertices = 0;
+        GLfloat vertices[1][2];
+
+        i32 pointSize = 5;
+
+        bool pressed[GLFW_KEY_LAST];
+};
+
+/* *****************************************************************************************************/
